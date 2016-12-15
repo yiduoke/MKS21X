9ae75d10956351e1c0905651adc273a3a550106d
yiduoke <yiduoke822@jhs185.org>
2016-12-14 20:43:33 -0500

    Add files via upload

--- 04Sorts/InsertionSort.java
+++ 04Sorts/InsertionSort.java
@@ -1,29 +1,26 @@
 public class InsertionSort{
-    public static String name(){return "06.Ke.Yiduo";}
-
-    public void add(int[] data, int index, int  element){
-	  int[] temp=new int[data.length+1];
-	  for (int i=0; i<data.length+1; i++){
-		  if (i==index){temp[i]=element;}
-		  else if(i<index){temp[i]=data[i];}
-		  else{temp[i]=data[i-1];}  	
+	public static String name(){
+	    return "06.Ke.Margaret"; 
 	  }
-	  data=temp;
-  }
-    public void remove(int[]data, int index){
-	  int[] x=data[index];
-	  for (int i=0; i<data.length-1; i++){
-		  if(i<index){data[i]=data[i];}
-		  else {data[i]=data[i+1];}
-	  }
-  }
-
     public static void insertionSort(int[] data){
-	for (int i=1; i<data.length; i++){
-	    for (int m=i-1;m>-1;m--){
-		if (data[i]<data[m]){add(data,m,data[i]);remove(data,i+1);}
-	    }
-	}
+    	for (int i=0; i<data.length-1; i++){
+    		for (int m=i; m>-1; m--){
+       			int x=data[data.length-1];
+    			if (x>=data[m]){
+    				for (int a=data.length-1;a>m+1;a--){
+    					data[a]=data[a-1];
+    				}
+    				data[m+1]=x;
+    				m=-1;
+    			}
+    			else if(m==0){
+    				for (int a=data.length-1;a>0;a--){
+    					data[a]=data[a-1];
+    				}
+    					data[0]=x;
+    			}
+    		}
+    	}
     }
 }
 
